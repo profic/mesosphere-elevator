5a33ea4ee10e148f88b8dc23d3313780d9d34b7a
John.Koepi <john.koepi@gmail.com>
2016-02-16 14:46:27 +0200

    separate elevator state, elevator self controller, distance helpers, goals with scores

--- src/main/scala/Elevator.scala
+++ src/main/scala/Elevator.scala
@@ -6,7 +6,14 @@ case object Up extends Direction
 case object Down extends Direction
 case object Idle extends Direction
 
-case class Task(to: Int)
+/**
+  * Goal describe demand to achieve certain floor
+  *
+  * @param to Target floor
+  * @param dir Reserved direction
+  * @param score Schedule score counted for this goal
+  */
+case class Goal(to: Int, dir: Direction, score: Double)
 
 // For the sake of simplicity it will be a trait implemented by the Intention,
 // which is an Order in mind of the Resident
@@ -29,24 +36,60 @@ case class Order(oid: Int, from: Int, to: Int, time: Int) extends PickupRequest 
   }
 }
 
-class Elevator(id: Int) {
+trait ElevatorState {
   var pos: Int = 0
   var orders: List[Order] = List()
-  var tasks: List[Task] = List()
 
-  def next = tasks.headOption
-  def idle = tasks.isEmpty
   def free = orders.isEmpty
 
   def distance(to: Int): Int = {
     math.abs(to - pos)
   }
 
-  def direction: Direction = {
-    tasks.headOption.map({ t =>
-      if (t.to - pos > 0) {
+  def distance(to: Goal): Int = {
+    distance(to.to)
+  }
+
+  def distance(to: PickupRequest): Int = {
+    distance(to.at)
+  }
+
+  def distance(to: Order): Int = {
+    if (orders.contains(to)) {
+      distance(to.to)
+    } else {
+      distance(to.at)
+    }
+  }
+
+  def goal(to: PickupRequest): Goal = Goal(to.at, to.direction, distance(to))
+
+  def goal(to: Order): Goal = {
+    if (orders.contains(to)) {
+      Goal(to.to, to.direction, distance(to.to))
+    } else {
+      goal(to.asInstanceOf[PickupRequest])
+    }
+  }
+}
+
+trait ElevatorController {
+  var tasks: List[Goal] = List()
+  var prev: Option[Goal] = None
+
+  def next = tasks.headOption
+  def idle = tasks.isEmpty
+  def floor: Int
+
+  def step(): Unit
+
+  def direction: Direction = direction(tasks.headOption)
+
+  def direction(goal: Option[Goal]): Direction = {
+    goal.map({ t =>
+      if (t.to - floor > 0) {
         Up
-      } else if (t.to - pos < 0) {
+      } else if (t.to - floor < 0) {
         Down
       } else {
         Idle
@@ -54,27 +97,36 @@ class Elevator(id: Int) {
     }).getOrElse(Idle)
   }
 
-  def step: Option[Task] = {
-    tasks.headOption.flatMap({ t =>
-      if (t.to - pos > 0) {
-        pos += 1
-        None
-      } else if (t.to - pos < 0) {
-        pos -= 1
-        None
-      } else {
-        tasks = tasks.tail
-        // Server all this floor orders
-        orders = orders.filter(_.to != pos)
-        Option(t)
-      }
-    })
-  }
-
-  private[this] def describeIntention = {
+  def describeIntention = {
     direction match {
       case Idle => "Idle"
       case x => s"going $x/$tasks"
+    }
+  }
+}
+
+class Elevator(id: Int) extends ElevatorState with ElevatorController {
+  override def floor = pos
+
+  override def step(): Unit = {
+    // Server all this floor orders - this is ideal world
+    orders = orders.filter(_.to != pos)
+
+    // If there are requests inside the elevator is busy
+    // If it's idle in that case, go for the closest
+    if (!free && idle) {
+        tasks = orders.sortBy(distance).headOption.map({ o => List(goal(o))}).getOrElse(List())
+    }
+
+    // Make a step
+    val d = direction
+    if (d == Up) {
+      pos += 1
+    } else if (d == Down) {
+      pos -= 1
+    } else if (tasks.nonEmpty) {
+      prev = Option(tasks.head)
+      tasks = tasks.tail
     }
   }
 
@@ -96,6 +148,13 @@ trait ControlSystem {
   // TODO Should provide Id, Orders, Tasks
   type ControlSystemStatus = Unit
 
+  def connect(to: Seq[Elevator]): Unit
+  def pickup(o: PickupRequest): Unit
+  def status: ControlSystemStatus
+  def step(): Unit
+}
+
+abstract class ControlSystemBase extends ControlSystem {
   // For the sake of simplicity we keep it here not to repeat this in Building
   var queue = new ListBuffer[PickupRequest]()
 
@@ -103,24 +162,22 @@ trait ControlSystem {
   // Or keeps their state in memory
   var elevators: Seq[Elevator] = List()
 
-  // System directly connected to the elevators controller via bus
-  def connect(to: Seq[Elevator]) = elevators = to
-  def pickup(o: PickupRequest) = queue += o
-  def status: ControlSystemStatus = {
-    println(s"Queue ${queue}")
+  override def connect(to: Seq[Elevator]) = elevators = to
+  override def pickup(o: PickupRequest) = queue += o
+  override def status: ControlSystemStatus = {
+    println(s"Queue $queue")
 
     for (e <- elevators) {
       println(e)
     }
   }
-  def step
 }
 
 // First-come First-served implementation of ControlSystem
-class FCFS extends ControlSystem {
+class FCFS extends ControlSystemBase {
   var pending = new ListBuffer[PickupRequest]()
 
-  override def step = {
+  override def step() = {
     // Server pending order
     // This algorithm so bad, that it do not allow to take ALL the orders at
     // this floor in order not to break FCFS guarantee
@@ -141,20 +198,15 @@ class FCFS extends ControlSystem {
       val elevator = free.head
       if (elevator.pos != task.at) {
         // Serve pickup
-        elevator.tasks = List(Task(task.at))
+        elevator.tasks = List(elevator.goal(task))
       }
       free = free.tail
       pending += task
     }
-
-    // Lift with orders
-    elevators.filter({ e => e.idle && !e.free }).foreach({ e =>
-      e.tasks = List(Task(e.orders.head.to))
-    })
   }
 
   override def status: ControlSystemStatus = {
-    println(s"Pending ${pending}")
+    println(s"Pending $pending")
     super.status
   }
 }
@@ -162,10 +214,10 @@ class FCFS extends ControlSystem {
 // Greedy algorithm and:
 // 1. Serve closest free lift
 // 2. Get people on the way
-class Improved extends ControlSystem {
+class Improved extends ControlSystemBase {
   var pending = new ListBuffer[PickupRequest]()
 
-  override def step = {
+  override def step() = {
     // Pickup destination floor
     elevators.filter({ e => e.idle && e.free }).foreach({ e =>
       pending.filter(_.at == e.pos).foreach({ o =>
@@ -200,7 +252,7 @@ class Improved extends ControlSystem {
       val e = free.head
       if (e.pos != task.at) {
         // Serve pickup
-        e.tasks = List(Task(task.at))
+        e.tasks = List(e.goal(task))
         // Group by Pickups to the pending, must keep same direction
         queue.filter({ t => t.at == task.at && t.direction == task.direction}).foreach({ o =>
           pending += o
@@ -215,9 +267,9 @@ class Improved extends ControlSystem {
     // Lift with orders
     elevators.filter({ e => !e.free }).foreach({ e =>
       // Greedy: go for closest distance
-      e.tasks = List(Task(e.orders.sortWith({
+      e.tasks = List(e.goal(e.orders.sortWith({
         (a, b) => math.abs(a.to - e.pos) < math.abs(b.to - e.pos)
-      }).head.to))
+      }).head))
     })
   }
 
@@ -251,20 +303,18 @@ class Building(floors: Int, elevators: Int, residents: Int) extends TimeMachine 
     clock += 1
 
     // Incoming residents
-    if (controller.queue.length < residents) {
-      val incoming = r.nextInt(incomingSpeed)
-      if (incoming > 0) {
-        val pr = newPickupRequest
-        println(s"Incoming (PickupRequest at ${pr.at}, ${pr.direction}})")
-        controller.pickup(pr)
-      }
+    val incoming = r.nextInt(incomingSpeed)
+    if (incoming > 0) {
+      val pr = newPickupRequest
+      println(s"Incoming (PickupRequest at ${pr.at}, ${pr.direction}})")
+      controller.pickup(pr)
     }
 
     // Tick controller
-    controller.step
+    controller.step()
 
     // Tick elevators
-    property.foreach(_.step)
+    property.foreach(_.step())
 
     status()
   }
